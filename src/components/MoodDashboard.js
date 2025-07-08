9767f8ea7c9009a5b79fabe2fd9d8b13ba7f83c1
aisha <aysha.alfassi@gmail.com>
2025-07-08 14:47:13 +0300

    feat: initial setup of MoodDashboard component with CSS Modules styling

---
+++ src/components/MoodDashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from '../styles/MoodDashboard.module.css';
+
+export default function MoodDashboard() {
+  return (
+    <div className={classes.container}>
+    <h2 className={classes.title}>Mood Dashboard</h2>
+  </div>
+  );
+}
